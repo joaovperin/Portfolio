fa76977983d5f1d1784c9f66d86fc80bd9690c4f
João Perin <joaovperin@live.com>
2018-06-10 19:15:28 -0300

    Added jQuery and created a non-user associated index page with a field to query for users.

--- src/main/java/br/jpe/portfy/ws/IndexController.java
+++ src/main/java/br/jpe/portfy/ws/IndexController.java
@@ -26,6 +26,11 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/")
 public class IndexController {
 
+    /**
+     * Mapping to the index URL
+     *
+     * @return ModelAndView
+     */
     @GetMapping
     public ModelAndView index() {
         Map map = new HashMap<>();
@@ -34,8 +39,35 @@ public class IndexController {
         return new ModelAndView("index", map);
     }
 
+    /**
+     * Checks if an user exists on database
+     *
+     * @param user
+     * @return ResponseEntity
+     */
+    @GetMapping("exists/{user}")
+    public ResponseEntity<String> checkUserExists(@PathVariable String user) {
+        // If user is not provided
+        if (user == null || user.trim().isEmpty()) {
+            return new ResponseEntity<>("User not provided.", HttpStatus.NOT_FOUND);
+        }
+        // User exists
+        if (true) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }
+        // User not found
+        return new ResponseEntity<>(String.format("User '%s' doesn't exists.", user), HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * Mapping to the index URL of an specific user
+     *
+     * @param user
+     * @return ModelAndView
+     * @throws br.jpe.portfy.exception.PortfyException
+     */
     @GetMapping("{user}")
-    public ModelAndView userIndex(@PathVariable String user) throws Exception {
+    public ModelAndView userIndex(@PathVariable String user) throws PortfyException {
         // TODO: Valid if the user exists and remove this .
         if (Arrays.asList("contact", "curriculum", "projects").contains(user)) {
             throw new PortfyException("Invalid URL!");
@@ -45,11 +77,6 @@ public class IndexController {
         map.put("header", " - INDEX -");
         map.put("currentUser", user);
         return new ModelAndView("index", map);
-    }
-
-    @GetMapping("hello")
-    public ResponseEntity<String> hello() {
-        return new ResponseEntity<>("I'm a bearpot.", HttpStatus.I_AM_A_TEAPOT);
     }
 
 }
--- src/main/resources/resources/js/common.js
+++ src/main/resources/resources/js/common.js
@@ -1,1 +1,55 @@
-console.log("Started :D");
+console.log("Application Started :D");
+
+/**
+ * Utilitary to 
+ * 
+ * @param {String} url
+ * @param {Object} customParam
+ * @returns {Object}
+ */
+var Ajax = function (url, customParam) {
+
+    // Validates the URL
+    if (!url)
+        throw 'URL Must be valid!';
+
+    // Some default parameters
+    var param = $.extend({
+        method: 'GET',
+        data: {}
+    }, customParam);
+
+    /**
+     * Sends a request and returns a promise
+     * 
+     * @returns {Promise}
+     */
+    function send() {
+        var q = $.Deferred();
+        $.ajax({
+            url: url,
+            method: param.method,
+            data: param.data,
+            success: (response) => q.resolve(response),
+            error: (response) => q.reject(response)
+        });
+        return q.promise();
+    }
+
+    function post() {
+        param.method = 'POST';
+        return send();
+    }
+
+    function get() {
+        param.method = 'GET';
+        return send();
+    }
+
+    return {
+        send: send,
+        post: post,
+        get: get
+    };
+
+};
